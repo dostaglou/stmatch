0906722d4b9ae8d375e32c53863d21f51c740df0
dillon wyatt <dostaglou@gmail.com>
2019-08-13 19:46:27 +0900

    made requested changes - has_many is top

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :courses
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
-  has_many :courses
 end
