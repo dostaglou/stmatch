ec438c2b32b1d87cfa6803392ada7923dc64c53e
dillon wyatt <dostaglou@gmail.com>
2019-08-13 19:04:43 +0900

    deleted old migration with USER and recreated it as USERS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_094730) do
+ActiveRecord::Schema.define(version: 2019_08_13_100350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
