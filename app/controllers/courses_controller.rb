eb5e8ee53f12e23e229b2477da89d751c6393739
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-14 12:14:36 +0900

    added courses index and show views. also fixed sign up page (added first and last name)

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,16 @@
 class CoursesController < ApplicationController
+  before_action :set_course
+
   def index
     @courses = Course.all
   end
 
   def show
-    @course = Course.find(:id)
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
   end
 end
