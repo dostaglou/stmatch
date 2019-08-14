07399ce98808860999cfe79660cf78d8c4c6f14e
dillon wyatt <dostaglou@gmail.com>
2019-08-14 12:57:25 +0900

    added NEW and CREATE methods to the COURSE Controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,11 +9,17 @@ class CoursesController < ApplicationController
   end
 
   def new
-
+    @course = Course.new
   end
 
   def create
-
+    @couse = Course.new(set_course_params)
+    @course.user = @user.id
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
   end
 
   private
@@ -25,5 +31,4 @@ class CoursesController < ApplicationController
   def set_course_params
     params.require(:course).permit(:name, :level, :duration, :description)
   end
-
 end
