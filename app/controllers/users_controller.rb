c21f0a7bcbd383decc1946798ed699d7c351f645
Sean Liu <syliu1998@gmail.com>
2019-08-15 16:41:59 +0900

    fix-image-upload-bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,15 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     authorize @user
-    if @user.update(set_booking_params)
-      redirect_to booking_path(@booking)
+    if @user.update(set_user_params)
+      redirect_to user_path(@user)
     else
       render :edit
     end
   end
 
   private
-  def set_booking_params
+  def set_user_params
     params.require(:user).permit(:photo)
   end
 end
