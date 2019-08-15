5b78a48ffe3b5d83837662bf498e06c85e968fd6
Geoffrey Wu <gwu20510@gmail.com>
2019-08-15 14:48:31 +0900

    added form to course show and flatpickr plugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
 import "bootstrap";
+import flatpickr from "flatpickr";
 
 import { initTooltipAvatar } from '../components/index';
+import { initFlatpickr } from '../components/flatpickr';
+
 initTooltipAvatar();
+initFlatpickr();
