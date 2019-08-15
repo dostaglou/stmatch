497403dc9a1e7f4a52e36779a89486eedfc89797
dillon wyatt <dostaglou@gmail.com>
2019-08-15 11:43:36 +0900

    indented view code, added 'has many' to user, coded logic for sifting bookings search by teacher and student

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,8 +5,19 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = policy_scope(Booking)
+    # below is all associated bookings
     @userbookings = @bookings.where(user: current_user)
-    # I think we will need to limit this to current user someway
+    # below is only the above's bookings as student if any
+    @student_bookings = []
+    # below is only the above's bookings as teacher if any
+    @teacher_bookings = []
+    @userbookings.each do |booking|
+      if booking.course.user_id == current_user.id
+        @teacher_bookings << booking
+      else
+        @student_bookings << booking
+      end
+    end
   end
   
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :courses
-
+  # The below gives us access to the bookings of users
+  has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
