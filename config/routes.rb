f58a35e2d8a01d28b5e80cdf587e98682b13151d
Sean Liu <syliu1998@gmail.com>
2019-08-15 15:53:28 +0900

    add-user-profile-image-not-done

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :reviews, only: [:index, :show]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
