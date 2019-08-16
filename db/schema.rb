cf313ae5e4346eaa82914f8b85abe2d6da753c35
Geoffrey Wu <gwu20510@gmail.com>
2019-08-16 12:05:07 +0900

    minor styling updates for cards and spacing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_15_070538) do
 
   # These are extensions that must be enabled in order to support this database
